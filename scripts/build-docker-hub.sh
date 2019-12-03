3fa9a9f0ab6f79c5aa7527521b9f3d85eeb96c77
juansantane <juan.santa@nebulae.com.co>
2019-12-03 16:07:08 -0500

    version 1.0.28

--- scripts/build-docker-hub.sh
+++ scripts/build-docker-hub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DOCKERHUB_IMAGE=keycloak-theme
-export DOCKERHUB_TAG=1.0.27
+export DOCKERHUB_TAG=1.0.28
 
 docker build  -t $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:$DOCKERHUB_TAG -t $DOCKERHUB_NAMESPACE/$DOCKERHUB_IMAGE:latest mods/
 docker login -u $DOCKERHUB_USER -p $DOCKERHUB_PASS
